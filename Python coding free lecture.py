fb6172e2ac917cc016eed35a7c5337cbfe174394
Jeongminseo <20181482@sch.ac.kr>
2024-03-15 03:04:47 +0900

    3m15d

--- Python coding free lecture.py
+++ Python coding free lecture.py
@@ -364,7 +364,34 @@ menu.add("생선까스") #오류
 name, age, hobby = ("김종국", 20, "코딩")
 print(name, age, hobby)
 
+# 3/14
+# 집합
+# 중복 안됨, 순서 없음
+my_set  = {1,2,3,3,3}
+print(my_set)
 
+java = {"유재석", "김태호", "양세형"}
+python = set(["유재석","박명수"])
+
+# 교집합 (java 와 python 을 모두 할 수 있는 개발자)
+print(java & python)
+print(java.intersection(python))
+
+# 합집합 (java 할 수 있거나 python 할 수 있는 개발자)
+print(java | python)
+print(java.union(python))
+
+# 차집합 (java 할 수 있지만 python 할 수 없음)
+print(java - python)
+print(java.difference(python))
+
+# python을 할 줄 아는 사람이 늘어남
+python.add("김태호")
+print(python)
+
+# java 를 까먹음
+java.remove("김태호")
+print(java)
 
 
 
