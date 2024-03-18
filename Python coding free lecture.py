5a27be1ec9238da1e9e815c527acc62a1351e75b
Jeongminseo <20181482@sch.ac.kr>
2024-03-18 20:12:39 +0900

    3m18d

--- Python coding free lecture.py
+++ Python coding free lecture.py
@@ -393,6 +393,63 @@ print(python)
 java.remove("김태호")
 print(java)
 
+#3/18
+# 자료구조의 변경
+# 커피숍
+menu = {"커피", "우유", "주스"}
+print(menu, type(menu))
 
+menu = list(menu)
+print(menu, type(menu))
+
+menu = tuple(menu)
+print(menu, type(menu))
+
+menu = set(menu)
+print(menu, type(menu))
+
+# quiz
+# 당신의 학교에서는 파이썬 코딩 대화를 주최합니다.
+# 참석률을 높이기 위해 댓글 이벤트를 진행하기로 하였습니다. 댓글 작성자들 중에 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰을 받게 됩니다.
+# 추첨 프로그램을 작성하시오
+
+#ex
+# from random import *
+# 1st = [1,2,3,4,5]
+# print(1st)
+# shuffle(1st)
+# print(1st)
+# print(sample(1st,1))
+
+# my answer code
+# from random import *
+# event = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+# shuffle(event)
+# event1 = sample(event,1)
+#
+# event = event.difference(event1)
+# shuffle(event)
+# event2 = sample(event,3)
+#
+# print("--당첨자 발표--" \n )
+# print(event2)
+
+# answer code
+from random import *
+users = range(1,21)
+# print(type(users))
+users = list(users)
+# print(type(users))
+
+print(users)
+shuffle(users)
+print(users)
+
+winners = sample(users, 4)
+
+print(" --당첨자 발표-- ")
+print("치킨 당첨자 : {0}".format(winners[0]))
+print("커피 당첨자 : {0}".format(winners[1:]))
+print(" -- 축하합니다 -- ")
 
 
