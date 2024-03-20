ff9962a716382742469b4161e4bcf60283ce2a9f
Jeongminseo <20181482@sch.ac.kr>
2024-03-20 14:02:46 +0900

    3m20d

--- Python coding free lecture.py
+++ Python coding free lecture.py
@@ -452,4 +452,148 @@ print("치킨 당첨자 : {0}".format(winners[0]))
 print("커피 당첨자 : {0}".format(winners[1:]))
 print(" -- 축하합니다 -- ")
 
+# 3/19
+# if
+weather = input("오늘 날씨는 어때요? ")
+if weather == "비" or weather == "눈":
+    print("우산을 챙기세요")
+elif weather == "미세먼지":
+    print("마스크를 챙기세요")
+else:
+    print("준비물이 필요 없어요")
+
+temp = int(input("기온은 어때요? ")) # 기온의 특징상 int를 붙임
+if 30 <= temp:
+    print("너무 더워요. 나가지 마세요")
+elif 10 <= temp and temp < 30:
+    print("괜찮은 날씨에요")
+elif 0 <= temp and temp < 10:
+    print("외투를 챙기세요")
+else:
+    print("너무 추워요 나가지 마세요")
+
+# # for
+# print("대기번호 : 1")
+# print("대기번호 : 2")
+# print("대기번호 : 3")
+# print("대기번호 : 4")
+
+# for waiting_no in [0, 1, 2, 3, 4]:
+#     print("대기번호 : {0}".format(waiting_no))
+for waiting_no in range(1, 6):
+    print("대기번호 : {0}".format(waiting_no))
+
+starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+for customer in starbucks:
+    print("{0}, 커피가 준비되었습니다.".format(customer))
+
+# while
+customer = "토르"
+index = 5
+while index >= 1:
+    print("{0},손님 커피가 준비되었습니다. {1} 번 남았어요.".format(customer, index))
+    index -= 1
+    if index == 0:
+        print("커피는 폐기처분되었습니다.")
+
+customer = "아이언맨"
+while True:
+    print("{0}, 커피가 준비 되었습니다. 호출 {1} 회".format(customer, index))
+    index += 1
+
+customer = "토르"
+person = "Unknown"
+
+while person != customer:
+    print("{0}, 커피가 준비 되었습니다.".format(customer))
+    person = input("이름이 어떻게 되세요?")
+
+# 3/20
+# continue and break
+absent = [2, 5] # 결석
+no_book = [7] # 책을 깜박함
+for student in range(1, 10):
+    if student in absent:
+        continue
+    elif student in no_book:
+        print("오늘 수업 여기까지. {0}는 교무실로 따라와".format(student))
+        break
+    print(" {0}, 책을 읽어봐".format(student))
+
+# 한줄 for
+# 출석번호 1, 2, 3, 4 앞에 100을 붙이기로 함 -> 101, 102, 103, 104
+students = [1, 2, 3, 4, 5]
+print(students)
+students = [i+100 for i in students]
+print(students)
+
+# 학생들 이름을 길이로 변환
+students = ["Iron man", "Thor", "Groot"]
+students = [len(i) for i in students]
+print(students)
+
+# 학생 이름을 대문자로 변환
+students = ["Iron man", "Thor", "Groot"]
+students = [i.upper() for i in students]
+print(students)
+
+# Quiz
+# 당신은 Cocoa 서비스를 이용하는 택시 기사님입니다.
+# 50명의 승객과 매칭 기회가 있을 때, 총 탑승 승객 수를 구하는 프로그램을 작성하시오.
+
+# my answer code
+for customers in range(1, 51):
+    time = randrange(5, 50)
+    if time <= 15:
+        check = "O"
+    else:
+        check = ""
+        print("[{0}] {1}번째 손님 (소요시간 : {2}분)".format(check, customers, time))
+        continue
+    print("총 탑승 승객 : {0} 분".format(check.count("O")))
+
+# answer code
+from random import *
+cnt = 0
+for i in range(1,51):
+    time = randrange(5, 51)
+    if 5 <= time <= 15:
+        print("[O] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+        cnt += 1
+    else:
+        print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+
+print("총 탑승 승객 : {0} 분".format(cnt))
+
+# 내 코드는 time도 잘못 설정, randrange도 잘못설정, 총 탑승 승객 수 파악 구현 실패
+
+# 함수
+def open_account():
+    print("새로운 계좌가 생성되었습니다.")
+# 이 상태에선 실행해도 아무런 변화가 없음
+open_account()
+# 함수 호출을 해줘야 실행됨
+
+# 전달값과 반환값
+def deposit(balance, money): #입금
+    print("입금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance + money))
+    return balance + money
+def withdraw(balance, money): #출금
+    if balance >= money:
+        print("출금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance - money))
+        return balance - money
+    else:
+        print("출금이 완료되지 않았습니다. 잔액은 {0} 원입니다.".format(balance))
+        return balance
+
+def withdraw_night(balance, money):
+    commission = 100
+    return commission ,balance - money - commission
+
+balance = 0
+balance = deposit(balance, 1000)
+# balance = withdraw(balance, 2000)
+# balance = withdraw(balance, 500)
+commission, balance = withdraw_night(balance, 500)
+print("수수료는 {0} 원이며, 잔액은 {1} 원입니다.".format(commission, balance))
 
