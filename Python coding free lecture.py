60e990bb14e70ec286f6b12e1f6eede1edb2ce8c
Jeongminseo <20181482@sch.ac.kr>
2024-03-22 23:07:32 +0900

    3m22d

--- Python coding free lecture.py
+++ Python coding free lecture.py
@@ -597,3 +597,16 @@ balance = deposit(balance, 1000)
 commission, balance = withdraw_night(balance, 500)
 print("수수료는 {0} 원이며, 잔액은 {1} 원입니다.".format(commission, balance))
 
+# 기본값
+# def profile(name, age, main_lang):
+#     print(f"이름 : {name}\t나이 : {age}\t주 사용 언어: {main_lang}")
+
+profile("유재석", 20, "파이썬")
+profile("김태호", 25, "자바")
+
+# 같은 학교 같은 학년 같은 반 같은 수업.
+def profile(name, age=17, main_lang="파이썬"):
+    print(f"이름 : {name}\t나이 : {age}\t주 사용 언어: {main_lang}")
+
+profile("유재석")
+profile("김태호")
