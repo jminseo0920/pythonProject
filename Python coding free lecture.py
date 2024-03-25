ef236f25bc28cfcd427e12809cc620b93f256d29
Jeongminseo <20181482@sch.ac.kr>
2024-03-25 20:05:46 +0900

    3m25d

--- Python coding free lecture.py
+++ Python coding free lecture.py
@@ -610,3 +610,34 @@ def profile(name, age=17, main_lang="파이썬"):
 
 profile("유재석")
 profile("김태호")
+
+# 3/25
+# 키워드값
+def profile(name, age, main_lang):
+    print(name, age, main_lang)
+
+profile(name = "유재석", main_lang = "파이썬", age = 20)
+profile(main_lang = "자바""김태호", age = 25, name = "김태호")
+
+# 가변인자
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print(f"이름 : {0}\t나이 : {1}\t".format(name, age), end = "")
+#     print(lang1, lang2, lang3, lang4, lang5)
+#
+def profile(name, age, *language):
+    print(f"이름 : {0}\t나이 : {1}\t".format(name, age), end="")
+    for lang in language:
+        print(lang, end="")
+    print()
+
+profile("유재석", 20, "Python", "Java", "C", "C++", "C#", "JavaScript")
+profile("김태호", 25, "Kotiln", "Swift")
+
+
+
+
+
+
+
+
+
